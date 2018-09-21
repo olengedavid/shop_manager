caeba9fb422d5531cbb7d096c6c6d4f9b29ea530
Gwaza <austinegwa64@gmail.com>
2018-09-21 07:42:58 +0300

    initial setup

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Login");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 750, 475));
         primaryStage.show();
     }
